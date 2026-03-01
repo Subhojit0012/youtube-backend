21b4eadf8d2d700078e528c7f1f6a9d51dfec7fc
Subhojit0012 <subhojitroy00012@gmail.com>
2026-03-01 10:51:08 +0530

    database connection file

---
+++ src/db/connect.db.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_STR as string);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { connectDB };
