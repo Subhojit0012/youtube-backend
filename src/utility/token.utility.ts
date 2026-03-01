bccdddfdafbaaac0e390e6a1a54c92e05bdae245
Subhojit0012 <subhojitroy00012@gmail.com>
2026-03-01 10:54:39 +0530

    add token utility functions for encoding and decoding JWTs

---
+++ src/utility/token.utility.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+export function encodeToken(userId: string) {
+  const token: any = jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET!,
+    { expiresIn: "1D" },
+    (error, token) => {
+      if (error) {
+        throw new Error("Error signing token");
+      }
+      return token;
+    },
+  );
+
+  if (!token || token instanceof Error) {
+    throw new Error("Error generating token");
+  }
+
+  return token;
+}
+
+export function decodeToken(token: string) {
+  try {
+    if(token === ""){
+        throw new Error("No token provided");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    return decoded;
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+}
