72d74434bd1680fa4c45fe7187eba027f120b476
Subhojit0012 <subhojitroy00012@gmail.com>
2026-03-28 20:57:26 +0530

    added playlist and video router

--- src/router/_app.router.ts
+++ src/router/_app.router.ts
@@ -1,6 +1,8 @@
-import { router, mergeRouters } from "./../utility/context.utility.js";
+import { mergeRouters } from "./../utility/context.utility.js";
 import { userRouter } from "./user.route.js";
+import playListRouter from "./playlist.route.js";
+import videoRouter from "./video.route.js";
 
-export const appRouter = mergeRouters(userRouter);
+export const appRouter = mergeRouters(userRouter, playListRouter, videoRouter);
 
 export type AppRouter = typeof appRouter;
