b5f8173bf3de2e435cb023bed7c7bca522c5f458
Subhojit0012 <subhojitroy00012@gmail.com>
2026-03-28 20:57:00 +0530

    minor update in video model

--- src/db/models/video.model.ts
+++ src/db/models/video.model.ts
@@ -2,16 +2,19 @@ import mongoose, { Schema } from "mongoose";
 
 export const videoSchema = new Schema(
   {
-    title: {type: String, required: true},
-    description: {type: String},
-    videoFile: {type: String, required: true},
-    thumbnail: {type: String, required: true},
+    title: { type: String, required: true },
+    description: { type: String },
+    videoFile: { type: String, required: true },
+    thumbnail: { type: String, required: true },
     meta: {
       likes: { type: Number, default: 0 },
       comments: {
         count: { type: Number, default: 0 },
         enabled: { type: Boolean, default: true },
-        userComments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+        userComments: [
+          { type: Schema.Types.ObjectId, ref: "Comment" },
+          { default: [] },
+        ],
       },
 
       views: {
@@ -19,7 +22,7 @@ export const videoSchema = new Schema(
         viewers: [{ type: Schema.Types.ObjectId, ref: "User" }],
       },
     },
-    owner: {type: Schema.Types.ObjectId, ref: "User"}
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true },
 );
