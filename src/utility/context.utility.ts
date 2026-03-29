ec436aef112377dc03f9fc9ea83cec212d8498bf
Subhojit0012 <subhojitroy00012@gmail.com>
2026-03-29 18:43:13 +0530

    refactor context utility to rename user property to payload for clarity

--- src/utility/context.utility.ts
+++ src/utility/context.utility.ts
@@ -1,22 +1,21 @@
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { decodeToken } from "./token.utility.js";
 import type { JwtPayload } from "jsonwebtoken";
 
 export interface ContextOption extends Partial<trpcExpress.CreateExpressContextOptions> {
-  user?: object | string | null;
+  payload?: JwtPayload | string | null;
 }
 
 export const createContext = ({
   req,
   res,
 }: trpcExpress.CreateExpressContextOptions): ContextOption => {
-  const auth: string =
-    req.headers.authorization?.split(" ")[1]?.toString() || "";
+  const auth: string = req.headers.authorization?.split(" ")[1]?.toString() || "";
   const decode = decodeToken(auth);
 
   if (decode) {
-    return { req, res, user: decode };
+    return { req, res, payload: decode };
   }
 
   return { req, res };
@@ -30,15 +29,10 @@ export const router = t.router;
 export const procedure = t.procedure;
 export const mergeRouters = t.mergeRouters;
 
-interface CreateInnerContextOptions extends Partial<trpcExpress.CreateExpressContextOptions> {
-  session?: object | null;
-  token?: string | JwtPayload
-}
+// set inner context for session
+// interface CreateInnerContextOptions extends Partial<trpcExpress.CreateExpressContextOptions> {
+//   session?: object | null;
+//   token?: string | JwtPayload | undefined;
+// }
 
-function createInnerContext(opts: CreateInnerContextOptions) {
-  return {
-    session: opts.session,
-    token: opts.token,
-    id: 1,
-  };
-}
+// function createInnerContext(opts: CreateInnerContextOptions) {}
