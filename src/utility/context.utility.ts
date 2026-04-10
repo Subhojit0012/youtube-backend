e1ba85f2ef5a2605295e51b1ec86b61b6f15af61
Subhojit0012 <subhojitroy00012@gmail.com>
2026-04-10 20:12:21 +0530

    L:38 added type annotaion to authProcedure

--- src/utility/context.utility.ts
+++ src/utility/context.utility.ts
@@ -1,10 +1,11 @@
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC, TRPCError, type ProcedureType } from "@trpc/server";
 import { decodeToken, encodeToken } from "./token.utility.js";
 import type { JwtPayload } from "jsonwebtoken";
 
 export interface ContextOption extends Partial<trpcExpress.CreateExpressContextOptions> {
   token?: string | null;
+  payload?: string | JwtPayload | null;
 }
 
 export const createContext = ({
@@ -13,7 +14,7 @@ export const createContext = ({
 }: trpcExpress.CreateExpressContextOptions): ContextOption => {
   const token: string =
     req.headers.authorization?.split(" ")[1]?.toString() || "";
-  
+
   if (token) {
     return { req, res, token: token };
   }
@@ -28,3 +29,23 @@ const t = initTRPC.context<Context>().create();
 export const router = t.router;
 export const procedure = t.procedure;
 export const mergeRouters = t.mergeRouters;
+
+/**
+ * protected procedure
+ */
+
+// removing *typeof procedure* will trow reference error because of @types/qs package so type annontaion is necessary
+export const authProcedure: typeof procedure = procedure.use((opts) => {
+  const { ctx, next } = opts;
+
+  const token = ctx.token ? ctx.token : "";
+
+  const decoded = decodeToken(token);
+
+  return next({
+    ctx: {
+      ...ctx,
+      payload: decoded,
+    },
+  });
+});
