c25a93b3d9c3d44bb64638f4b17be1df0d625049
Subhojit0012 <subhojitroy00012@gmail.com>
2026-04-10 20:12:58 +0530

    linting -___-

--- src/router/_app.router.ts
+++ src/router/_app.router.ts
@@ -2,7 +2,13 @@ import { mergeRouters } from "./../utility/context.utility.js";
 import { userRouter } from "./user.route.js";
 import playListRouter from "./playlist.route.js";
 import videoRouter from "./video.route.js";
+import historyRouter from "./history.route.js";
 
-export const appRouter = mergeRouters(userRouter, playListRouter, videoRouter);
+export const appRouter = mergeRouters(
+  userRouter,
+  playListRouter,
+  videoRouter,
+  historyRouter,
+);
 
 export type AppRouter = typeof appRouter;
