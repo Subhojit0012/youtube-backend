d0b0e66cab208652370c432e393172f351e269a1
Subhojit0012 <subhojitroy00012@gmail.com>
2026-04-10 20:16:45 +0530

    implemnting methods(instance/static)

--- src/db/models/playlist.model.ts
+++ src/db/models/playlist.model.ts
@@ -10,4 +10,35 @@ export const playlist = new Schema(
   { timestamps: true },
 );
 
+// methods implementation
+playlist.methods.addToPlaylist = function (
+  videoId?: mongoose.Types.ObjectId,
+  userId?: mongoose.Types.ObjectId,
+) {
+  if (!videoId && !userId)
+    return new Error("VIDEO ID AND USER ID ARE REQUIRED");
+
+  if (this.owner.toString() !== userId?.toString()) {
+    throw new Error("UNAUTHORIZED");
+  }
+
+  this.contents.push(videoId);
+};
+
+playlist.methods.removeFromPlaylist = function (
+  videoId?: mongoose.Types.ObjectId,
+  userId?: mongoose.Types.ObjectId,
+) {
+  if (!videoId && !userId)
+    return new Error("VIDEO ID AND USER ID ARE REQUIRED");
+
+  if (this.owner.toString() !== userId?.toString()) {
+    throw new Error("UNAUTHORIZED");
+  }
+
+  this.contents = this.contents.filter(
+    (item: mongoose.Types.ObjectId) => item.toString() !== videoId?.toString(),
+  );
+};
+
 export const Playlist = mongoose.model("Playlist", playlist);
