c61a5efeaddab0c92c9ad57a7f1c896623163e63
Subhojit0012 <subhojitroy00012@gmail.com>
2026-04-21 23:38:36 +0530

    resolved the type annotaion problem

--- src/service/playlist.service.ts
+++ src/service/playlist.service.ts
@@ -1,22 +1,41 @@
 import type { JwtPayload } from "jsonwebtoken";
 import { Playlist } from "../db/models/playlist.model.js";
-import type { ObjectId } from "mongoose";
+import type { mongo, ObjectId } from "mongoose";
+import mongoose from "mongoose";
 
-interface Playlist {
+interface CreatePlaylistOpts {
   input: {
-    videoId: string | ObjectId;
-    playListId?: string | ObjectId;
-    name?: string;
+    videoId: mongoose.Types.ObjectId;
+    playListId?: mongoose.Types.ObjectId;
+    name: string;
   };
   ctx: {
     payload: {
-      id: string | ObjectId;
+      id: mongoose.Types.ObjectId;
     };
   };
 }
+// check if the playlist is exists for the user
+// if exits then add the video to the
+async function createPlaylist(opts: CreatePlaylistOpts) {
+  const { input, ctx } = opts;
 
-function playlistService(opts: Playlist) {
-  return Promise.resolve();
+  let checkPlaylist = await checkUserPlaylist(ctx.payload?.id);
+
+  if (checkPlaylist) {
+    checkPlaylist.addToPlaylist(input.videoId, ctx.payload?.id);
+  }
+
+  // create a new playlist
+  await Playlist.createPlaylist(input.name, ctx.payload?.id);
 }
 
-export { playlistService };
+export { createPlaylist };
+
+async function checkUserPlaylist(userId: mongoose.Types.ObjectId) {
+  const playlist = await Playlist.findOne({ owner: userId });
+
+  if (playlist) return playlist;
+
+  return false;
+}
