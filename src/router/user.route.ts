b4e844360bf1836043449ded87c9d128b1d5d1ac
Subhojit0012 <subhojitroy00012@gmail.com>
2026-04-30 21:07:45 +0530

    added auth procedure to update, delete and getUserData route and also created delete user route

--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -2,8 +2,9 @@ import { User } from "../db/models/user.model.js";
 import { router, procedure } from "./../utility/context.utility.js";
 import { TRPCError } from "@trpc/server";
 import { string, z } from "zod";
-import { createUser, login, updateUser } from "../service/user.service.js";
+import { createUser, deleteUser, login, updateUser } from "../service/user.service.js";
 import jwt from "jsonwebtoken";
+import { authProcedure } from "./../utility/context.utility.js";
 
 export const userRouter = router({
   // /signup route
@@ -13,32 +14,38 @@ export const userRouter = router({
       const id = await createUser(input);
 
       // Generate JWT token and set it in the response header
-      ctx.res?.setHeader("Authorization", "Bearer " + jwt.sign({id: id}, process.env.JWT_SECRET!, { expiresIn: "1D"}))
+      ctx.res?.setHeader(
+        "Authorization",
+        "Bearer " +
+          jwt.sign({ id: id }, process.env.JWT_SECRET!, { expiresIn: "1D" }),
+      );
     }),
 
   //login
 
   login: procedure
     .input(z.object({ email: string(), password: string() }))
-    .mutation(async ({ input }) => {
-      await login(input);
+    .mutation(async ({ input, ctx }) => {
+      const _id = await login(input);
+
+      ctx.res?.setHeader(
+        "Authorization",
+        "Bearer " +
+          jwt.sign({ id: _id }, process.env.JWT_SECRET!, { expiresIn: "1D" }),
+      );
+
+      return { message: "Login successful" };
     }),
   // /deleteUser route
-  // updateUser route
-  update: procedure
-    .input(
-      z.object({
-        name: string(),
-        email: string(),
-        password: string(),
-      }),
-    )
-    .mutation(async ({ input }) => {
-      await updateUser(input);
-    }),
-  // /getUserById route
+  deleteUser: authProcedure.input(string()).mutation(async ({ input, ctx }) => {
+    if (
+      ctx.payload === undefined ||
+      typeof ctx.payload !== "object" ||
+      ctx.payload === null
+    ) {
+      throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid token" });
+    }
 
-  getUserById: procedure.input(string()).query(async ({ input }) => {
     if (!input) {
       throw new TRPCError({
         code: "BAD_REQUEST",
@@ -47,7 +54,48 @@ export const userRouter = router({
       });
     }
 
-    const user = await User.findById({ _id: input });
+    await deleteUser({ userId: input });
+  }),
+  // updateUser route
+  update: authProcedure
+    .input(
+      z.object({
+        name: string(),
+        email: string(),
+        password: string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      if (
+        ctx.payload === undefined ||
+        typeof ctx.payload !== "object" ||
+        ctx.payload === null
+      ) {
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid token" });
+      }
+
+      await updateUser(input);
+    }),
+  // /getUserById route
+
+  getUserById: authProcedure.input(string()).query(async ({ input, ctx }) => {
+    if (
+      ctx.payload === undefined ||
+      typeof ctx.payload !== "object" ||
+      ctx.payload === null
+    ) {
+      throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid token" });
+    }
+
+    if (!input) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "User ID is required",
+        cause: "Invalid input",
+      });
+    }
+
+    const user = await User.findById({ _id: ctx.payload.id });
 
     if (!user) {
       throw new TRPCError({
