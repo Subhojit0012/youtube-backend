37e37d20193234c8535ec70bbb9f7df2936f7851
Subhojit0012 <subhojitroy00012@gmail.com>
2026-05-19 11:58:43 +0530

    get all history

--- src/router/history.route.ts
+++ src/router/history.route.ts
@@ -1,7 +1,7 @@
 import { authProcedure } from "../utility/context.utility.js";
 import { router } from "../utility/context.utility.js";
 import z from "zod";
-import historyService from "../service/history.service.js";
+import { historyService, getHistory} from "../service/history.service.js";
 
 const historyRouter = router({
     addToHistory: authProcedure.input(
@@ -19,6 +19,17 @@ const historyRouter = router({
         }
         await historyService(userId, videoId);
         return { message: "Video added to history" };
+    }),
+
+    getHistory: authProcedure.query(async ({ ctx }) => {
+        const userId =
+            typeof ctx.payload === "object" && ctx.payload !== null
+                ? (ctx.payload as {id: string}).id
+                : undefined;
+        if (!userId) {
+            throw new Error("UNAUTHORIZED");
+        }
+        return await getHistory(userId);
     }),
 });
 
