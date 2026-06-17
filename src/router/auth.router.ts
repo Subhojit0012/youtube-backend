99c21c637acdbf7f9f789fbf7d8f25153fee054f
Subhojit0012 <subhojitroy00012@gmail.com>
2026-06-17 20:46:07 +0530

    added comment

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -4,6 +4,12 @@ import { encodeToken } from "../utility/token.utility.js";
 
 const router = Router();
 
+/* 
+
+implement the authentication procedure in express router e.g. login, signup, so when the token will be created or updated we can set it to the /trpc middleware header so the tRPC Context can get the JWT token, but if the token is not found in the request header in /trpc/* route it will thorw error by the trpc protected-procedure.
+
+*/
+
 router.post("/signup", async (req, res, next) => {
   const { name, email, password } = req.body;
   const id = await createUser({ name, email, password });
