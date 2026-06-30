4d284a1cc9e217e8e2518bbb4d473cf67cf84ed0
Subhojit0012 <subhojitroy00012@gmail.com>
2026-06-30 19:34:27 +0530

    refactor: format code for consistency in history route

--- src/router/history.route.ts
+++ src/router/history.route.ts
@@ -1,40 +1,42 @@
 import { authProcedure } from "../utility/context.utility.js";
 import { router } from "../utility/context.utility.js";
 import z from "zod";
-import { getHistory, addToHistory} from "../service/history.service.js";
+import { getHistory, addToHistory } from "../service/history.service.js";
 
 const historyRouter = router({
-    addToHistory: authProcedure.input(
-        z.object({
-            videoId: z.string(),
-        }),
-    ).mutation(async ({ input, ctx }) => {
-        const { videoId } = input;
-        const userId =
-            typeof ctx.payload === "object" && ctx.payload !== null
-                ? (ctx.payload as {id: string}).id
-                : undefined;
-        if (!userId) {
-            throw new Error("UNAUTHORIZED");
-        }
-        await addToHistory(userId, videoId);
+  addToHistory: authProcedure
+    .input(
+      z.object({
+        videoId: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { videoId } = input;
+      const userId =
+        typeof ctx.payload === "object" && ctx.payload !== null
+          ? (ctx.payload as { id: string }).id
+          : undefined;
+      if (!userId) {
+        throw new Error("UNAUTHORIZED");
+      }
+      await addToHistory(userId, videoId);
 
-        return {message: "Video added to history"};
+      return { message: "Video added to history"};
     }),
 
-    getHistory: authProcedure.query(async ({ ctx }) => {
-        const userId =
-            typeof ctx.payload === "object" && ctx.payload !== null
-                ? (ctx.payload as {id: string}).id
-                : undefined;
-        if (!userId) {
-            throw new Error("UNAUTHORIZED");
-        }
-       
-        const history = await getHistory(userId);
+  getHistory: authProcedure.query(async ({ ctx }) => {
+    const userId =
+      typeof ctx.payload === "object" && ctx.payload !== null
+        ? (ctx.payload as { id: string }).id
+        : undefined;
+    if (!userId) {
+      throw new Error("UNAUTHORIZED");
+    }
 
-        return {history};
-    }),
+    const history = await getHistory(userId);
+
+    return { history };
+  }),
 });
 
 export default historyRouter;
