c4e43978674caafdcc8287a4cf0c1572fcda424e
Subhojit0012 <subhojitroy00012@gmail.com>
2026-09-20 19:12:35 +0530

    refactor: remove unused historyService function from history service

--- src/service/history.service.ts
+++ src/service/history.service.ts
@@ -1,18 +1,6 @@
 import { History } from "../db/models/history.model.js";
 import { TRPCError } from "@trpc/server";
 
-async function historyService(userId: string, videoId: string) {
-  // first check if the video is already included in history if yes then update the timestamps
-  // if not then add it to the history
-
-  const history = await History.findOne({ userId });
-
-  const video = history?.videoId.find((item) => item.toString() === videoId);
-
-  if (!video) {
-    throw new TRPCError({ code: "BAD_REQUEST", message: "Invalid input" });
-  }
-}
 
 export async function addToHistory(userId: string, videoId: string) {
   const existingHistory = await History.findOne({ userId });
